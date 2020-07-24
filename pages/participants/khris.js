2fb2c9fdf8139c948391ffb62e85e2a74102d798
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-24 13:15:22 -0500

    added my name and test

---
+++ pages/participants/khris.js
@@ -0,0 +1,16 @@
+import Layout from '../../components/Layout';
+import About from '../../components/About';
+
+function Khris() {
+  return (
+    <Layout>
+      <About
+        name='Khris Punzalan'
+        imageUrl='https://images.unsplash.com/photo-1501555088652-021faa106b9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3153&q=80'
+        description='Hey, there! My name is Khris and this is my first open source contribution. I love hiking and being outdoors!'
+      />
+    </Layout>
+  );
+}
+
+export default Khris;
