9bca0bd62b5bc1d85267526bc5a2fa445487974f
dlm19 <57910323+dlm19@users.noreply.github.com>
2020-07-24 17:23:07 -0400

    feature: added dani to participant list (#53)

---
+++ pages/participants/dani.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout";
+import About from "../../components/About";
+
+function Dani() {
+  return (
+    <Layout>
+      <About
+        name="Dani Meyer"
+        imageUrl="https://www.testuff.com/wp-content/uploads/open-source.png"
+        description="Hi, I'm Dani. I'm excited to be working on my first open source contribution!"
+      />
+    </Layout>
+  );
+}
+
+export default Dani;
