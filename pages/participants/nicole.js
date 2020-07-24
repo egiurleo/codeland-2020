8cadcf65f17c5c18c764f1b92a8061e999efb304
Nicole Lucien <51276888+nicmlu@users.noreply.github.com>
2020-07-24 17:14:03 -0400

    add Nicole participant page (#57)

---
+++ pages/participants/nicole.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout";
+import About from "../../components/About";
+
+function Nicole() {
+    return (
+        <Layout>
+            <About
+                name="Nicole Lucien"
+                imageUrl="https://cdn.dribbble.com/users/878959/screenshots/4014106/in-the-life-of-a-twin-mom.png"
+                description="Hi! My name is Nicole and I'm a student at Flatiron School. I love writing Ruby code and being a mom to my Twins."
+            />
+        </Layout>
+    );
+}
+
+export default Nicole;
