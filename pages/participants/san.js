a1436673b0b1904300c8713d59cfa49eca9cec27
Santhosh B. Appan <12586088+sbappan@users.noreply.github.com>
2020-07-24 12:56:57 -0400

    Added participant page for Santhosh (#31)

---
+++ pages/participants/san.js
@@ -0,0 +1,16 @@
+import Layout from '../../components/Layout';
+import About from '../../components/About';
+
+function San() {
+  return (
+    <Layout>
+      <About
+        name="Santhosh Appan"
+        imageUrl="https://images.unsplash.com/photo-1595323397978-65433d24fc23"
+        description='Hey! I am Santhosh Appan, and I am a participant in the "Your First Open Source Contribution" workshop. I enjoy building front end web apps and reading novels.'
+      />
+    </Layout>
+  );
+}
+
+export default San;
