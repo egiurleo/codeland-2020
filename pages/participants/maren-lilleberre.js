8a54ecc54157f875b55963f1f8214a09a29c7b67
Maren Lilleberre <marenlilleberre@gmail.com>
2020-07-24 16:43:40 +0200

    add page and link maren-lilleberre

---
+++ pages/participants/maren-lilleberre.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout";
+import About from "../../components/About";
+
+function Emily() {
+  return (
+    <Layout>
+      <About
+        name="Maren Lilleberre"
+        imageUrl="https://github.com/Zohla/Zohla/blob/master/maren_bulb.jpg"
+        description="I'm a mother of 3, a preschool teacher by day, and a codenewbie at night(you know, after the kids bedtime and also every other free time I can squeeze in!)"
+      />
+    </Layout>
+  );
+}
+
+export default Emily;
