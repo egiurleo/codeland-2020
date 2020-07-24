de35f4e143eb13bcf6b165f2a7d9f6734aa9a991
Aimeri Baddouh <aimeri.baddouh@serviceprosig.com>
2020-07-24 09:58:01 -0400

    Add Aimeri to the list of participants

---
+++ pages/participants/aimeri.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout";
+import About from "../../components/About";
+
+function Aimeri() {
+  return (
+    <Layout>
+      <About
+        name="Aimeri Baddouh"
+        imageUrl="https://images.unsplash.com/photo-1595597764845-548b7c1233a5"
+        description="Hi, Aimeri here. Writting code by day, making espresso by night, and regretting it the next day."
+      />
+    </Layout>
+  );
+}
+
+export default Aimeri;
