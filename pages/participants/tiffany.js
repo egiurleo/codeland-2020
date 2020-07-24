6b0b9d579a24fbf44ef407de15b950a0029b8797
Tiffany <tdthompson899@gmail.com>
2020-07-24 16:23:58 -0500

    Add Tiffany to participant list (#52)
    Co-authored-by: Tiffany <tiffany@Tiffanys-MacBook-Pro.local>

---
+++ pages/participants/tiffany.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout";
+import About from "../../components/About";
+
+function Tiffany() {
+  return (
+    <Layout>
+      <About
+        name="Tiffany"
+        imageUrl="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beach-quotes-1559667853.jpg?crop=1.00xw:0.751xh;0,0.202xh&resize=1200:*"
+        description="Hey, I'm Tiffany. I'm a backend engineer at a music licensing company. In my spare time, I make youtube videos and enjoy spending time with my family."
+      />
+    </Layout>
+  );
+}
+
+export default Tiffany;
