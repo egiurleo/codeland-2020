6132c5358d231d4ed9ac6cfe39a077ae36fc1fbb
Ceora Ford <ceoraford@Ceoras-MBP.home>
2020-07-24 14:16:11 -0400

    Add Ceora's name to home page list

---
+++ pages/participants/ceora.js
@@ -0,0 +1,16 @@
+import Layout from "../../components/Layout";
+import About from "../../components/About";
+
+function Ceora() {
+  return (
+    <Layout>
+      <About
+        name="Ceora Ford"
+        imageUrl="https://images.unsplash.com/photo-1519052537078-e6302a4968d4"
+        description="Hey, there! My name is Ceora"
+      />
+    </Layout>
+  );
+}
+
+export default Ceora;
